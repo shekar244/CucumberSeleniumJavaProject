632a5efe063db30336793d50837252ac42d0c817
Shekar Reddy <Shekaryedma@outlook.com>
2025-01-06 12:11:05 -0500

    updates

---
+++ src/main/java/com/ysr/Exercises/StreamApiTopics/StreamApiLists.java
@@ -0,0 +1,21 @@
+package com.ysr.Exercises.StreamApiTopics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamApiLists {
+    public static void main(String[] args) {
+        String[] array = new String[20];
+        //array[0]="Shekar";
+        int len = array.length;
+        System.out.println(len);
+        StringBuffer sb = new StringBuffer("Shekar ");
+        sb.append("Reddy");
+        System.out.println(sb);
+        List<String> ls = new ArrayList<>();
+
+
+
+
+    }
+}
